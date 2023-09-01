9a39f488488dbb9520dcd595873f2f14d3360d90
MessicaH <135277410+MessicaH@users.noreply.github.com>
2023-09-01 00:58:15 -0500

    DOM follow-along and new edits for Aug 31

--- jhelm.js
+++ jhelm.js
@@ -1,5 +1,21 @@
-// Get current date for website footer - from Sololearn Code Bits
-function printTime() {
+// Function to get the current date and time
+function getCurrentDateAndTime() {
+    const dateTime = new Date();
+    console.log(dateTime);
+    return dateTime.toLocaleString();
+  }
+  
+  // Target an HTML element to display the current date and time
+  const dateDisplay = document.getElementById("date-container");
+  
+  // Set the innerHTML of the element to the current date and time returned by the function
+  dateDisplay.innerHTML = getCurrentDateAndTime();
+  
+
+
+
+/* Get current date for website footer - from Sololearn Code Bits /////////////////////
+function getTime() {
     let d = new Date();
     let hours = d.getHours();
     let mins = d.getMinutes();
@@ -30,7 +46,35 @@ function printTime() {
     //console.log((hours/2)+":"+mins+":"+secs+dayTime);
     //console.log(hours+":"+mins+":"+secs);
 }
-setInterval(printTime, 1000);
-console.log(printTime);
+setInterval(getTime, 1000);
+//console.log(getTime);
 
-document.getElementById("currentDate").innerHTML = printTime
+const printDate = document.getElementById("site-date")
+const currentDate = document.createElement("getTime")
+*/
+
+
+
+// Have new tabs open from links clicked ////////////////////////////
+/*
+let newTab = document.getElementsByTagName("href");
+
+for (var i = 0; i < newTab.length; i++) {
+    newTab[i].setAttribute('target', '_blank');
+}
+*/
+
+
+
+/* Have table on DOM page do a thing //////////////////////////////////////
+
+function addToTable() {
+    // Get what's in the text box
+
+    // Make a new <tr> with info in <td> cells from data in text field
+
+    // Add item to the table
+
+    // Clear out text field after entry
+}
+*/
